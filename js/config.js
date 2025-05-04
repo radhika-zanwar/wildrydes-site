d646ecab76e157c1488d4ea985e5c25bab5cac63
radhika-zanwar <122522127+radhika-zanwar@users.noreply.github.com>
2025-05-04 13:39:36 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_3S3WKBhXD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ja8p5tu6ovjmdvr66equdht4h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
